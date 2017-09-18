a3191c206d969e1b32409d85e18bd93f619e0517
Lê Anh Tuấn <leanhtuan1996@users.noreply.github.com>
2017-09-18 22:39:41 +0700

    Add Accept & reject presrioption for admin role

---
+++ Pharmacy/Classes/UI/Admin/Drugs/DrugCell/DrugAdminCell.swift
@@ -0,0 +1,24 @@
+//
+//  DrugAdminCell.swift
+//  Pharmacy
+//
+//  Created by Lê Anh Tuấn on 9/18/17.
+//  Copyright © 2017 Lê Anh Tuấn. All rights reserved.
+//
+
+import UIKit
+
+class DrugAdminCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Pharmacy/Classes/UI/Admin/Prescriptions/PrescriptionsAdminCell/PrescriptionsAdminCell.swift
@@ -0,0 +1,24 @@
+//
+//  PrescriptionsAdminCell.swift
+//  Pharmacy
+//
+//  Created by Lê Anh Tuấn on 9/18/17.
+//  Copyright © 2017 Lê Anh Tuấn. All rights reserved.
+//
+
+import UIKit
+
+class PrescriptionsAdminCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
